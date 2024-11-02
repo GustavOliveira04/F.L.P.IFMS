e2d337520273f3bff9465d9350a0e3085a949c99
GustavOliveira04 <gufreitas254@gmail.com>
2024-11-01 21:00:53 -0400

    Desafios

--- Desafios/desafio4.py
+++ Desafios/desafio4.py
@@ -2,8 +2,8 @@ from sys import stdin
 
 for line in stdin:
     line = line.split()
-    a = int(line(0))
-    b = int(line(1))
+    a = int(line[0])
+    b = int(line[1])
     print(abs(a - b)) 
 
     #abs mostra apenas a versão positiva do numero(versão ABSoluta).
